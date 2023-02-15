e37b25852a95991b5bafcf9da21f1df5e5398bda
natainditama <natainditama@gmail.com>
2023-02-15 20:43:49 +0800

    refactor: Update HomeController

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -2,38 +2,38 @@
 
 namespace NataInditama\Auctionx\Controllers;
 
+use NataInditama\Auctionx\App\Auth;
 use NataInditama\Auctionx\App\View;
-use NataInditama\Auctionx\Models\Barang;
+use NataInditama\Auctionx\Models\Lelang;
+use NataInditama\Auctionx\Models\Masyarakat;
+use NataInditama\Auctionx\Models\Petugas;
 
 class HomeController
 {
-  public Barang $product;
+  public ?array $session = null;
+  public Lelang $auction;
 
   public function __construct()
-  {
-    $this->product = new Barang();
+  {    
+    $auth = Auth::getSession();
+    $this->auction = new Lelang();
+    
+    if (isset($auth)) {
+      $user = $auth['level'] == "masyarakat" ?  new Masyarakat() : new Petugas();
+      $user = (array) $user->findByUsername($auth['username']);
+      $user['level'] = $auth['level'];
+      $this->session = $user;
+    }
   }
 
   public function index(): void
   {
-    View::render("home/index");
+    $auctions = $this->auction->findAll();
+
+    View::render("home/index", [
+      'products' => $auctions,
+      "auth" => $this->session,
+    ]);      
   }
   
-  public function detail(string $id): void
-  {
-    var_dump($this->product->findById($id));
-    View::render("home/index", [
-      "title" => $id
-    ]);
-  }
-
-  public function admin(): void
-  {
-    var_dump("admin");
-  }
-
-  public function login(): void
-  {
-    var_dump("login");
-  }
 }
