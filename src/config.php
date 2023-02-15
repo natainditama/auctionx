6300aa753cdd326d33c00b234d1eb8b21980df57
natainditama <natainditama@gmail.com>
2023-02-16 06:17:38 +0800

    fix: Update database config

--- src/config.php
+++ src/config.php
@@ -7,5 +7,8 @@ define("SITE_BASE_URL", "http://localhost/auctionx/public/");
 // Database
 define("DB_HOST", "localhost");
 define("DB_USER", "root");
-define("DB_PASS", "root");
+define("DB_PASS", "");
 define("DB_NAME", "auctionx");
+
+// Timezone
+date_default_timezone_set("Asia/Makassar");
