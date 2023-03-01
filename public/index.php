1dd8b029b18a8d739ef1a21ba9b740bb0e1b9670
natainditama <natainditama@gmail.com>
2023-03-01 09:09:06 +0800

    feat: Update auth route

--- public/index.php
+++ public/index.php
@@ -5,13 +5,19 @@ require_once __DIR__ . "/../src/config.php";
 require_once __DIR__ . "/../vendor/autoload.php";
 
 use NataInditama\Auctionx\App\Router;
+use NataInditama\Auctionx\Controllers\AuthController;
 use NataInditama\Auctionx\Controllers\HomeController;
 use NataInditama\Auctionx\Middleware\AuthMiddleware;
 use NataInditama\Auctionx\Middleware\GuestMiddleware;
 
 Router::add(path: '/', controller: HomeController::class, function: 'index');
-Router::add(path: '/detail/([0-9a-zA-Z]*)', controller: HomeController::class, function: 'detail');
-Router::add(path: '/admin', controller: HomeController::class, function: 'admin', middleware: [GuestMiddleware::class]);
-Router::add(path: '/login', controller: HomeController::class, function: 'login', middleware: [AuthMiddleware::class]);
+
+// Auth Routes
+Router::add(path: '/logout', controller: AuthController::class, function: 'logout', middleware: [GuestMiddleware::class]);
+Router::add(path: '/login', controller: AuthController::class, function: 'login', middleware: [AuthMiddleware::class]);
+Router::add(path: '/register', controller: AuthController::class, function: 'register', middleware: [AuthMiddleware::class]);
+Router::add(path: '/admin', controller: AuthController::class, function: 'adminRegister');
+Router::add(path: '/login', controller: AuthController::class, function: 'postLogin', method: "POST");
+Router::add(path: '/register', controller: AuthController::class, function: 'postRegister', method: "POST");
 
 Router::run();
