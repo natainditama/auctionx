efb4f84fefe2eab93fe3c8f196b93cc9c1e63b0c
Nata Inditama <natainditama@gmail.com>
2023-04-22 17:24:39 +0800

    fix: Redirect public page

--- index.php
+++ index.php
@@ -1,26 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-  <?php 
-    $page = $_GET['p'] ?? "/";
-    switch ($page) {
-      case '/':
-        echo "home";
-        break;
-      case '/about':
-        echo "about";
-        break;
-      default:
-        echo "404";
-        break;
-    }
-  
-  ?>
-</body>
-</html>
+<?php 
+header("Location: ./public");
+?>
