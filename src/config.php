b7f5e5977bd1cfd08454265469b6861791598f7f
Nata Inditama <natainditama@gmail.com>
2023-04-22 17:29:02 +0800

    feat(config): Update site configuration

--- src/config.php
+++ src/config.php
@@ -1,11 +1,14 @@
-<?php 
+<?php
 
 // Site
 define("SITE_NAME", "Auctionx");
-define("SITE_BASE_URL", "http://localhost/auctionx/public/");
+define("SITE_BASE_URL", "http://localhost/web_027_nata_p4/public/");
 
 // Database
 define("DB_HOST", "localhost");
 define("DB_USER", "root");
 define("DB_PASS", "");
-define("DB_NAME", "auctionx");
+define("DB_NAME", "db_027_nata_p4");
+
+// Timezone
+date_default_timezone_set("Asia/Makassar");
