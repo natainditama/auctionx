92b7dbc282cbc58bb0c3d535361d2415670f32fc
Nata Inditama <natainditama@gmail.com>
2023-04-23 14:44:14 +0800

    style: Update page layout 404

--- src/Views/404.php
+++ src/Views/404.php
@@ -1,1 +1,15 @@
-<h1>404</h1>
+<div class="container d-flex justify-content-center align-items-center">
+  <div class="row">
+    <div class="col-12">
+      <div class="text-center">
+        <div class="mb-3">
+          <img src="assets/images/error/404-error-img.png" alt="" class="img-fluid">
+        </div>
+        <h1 class="display-4 fw-bold">Oops! the page not found.</h1>
+        <p class="mb-4">Or simply leverage the expertise of our consultation
+          team.</p>
+        <a href="./" class="btn btn-primary">Go Home</a>
+      </div>
+    </div>
+  </div>
+</div>
