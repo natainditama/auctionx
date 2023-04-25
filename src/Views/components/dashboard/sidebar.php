73cb1bba7b02c3c87a6f6259b5044e24909df5bb
Nata Inditama <natainditama@gmail.com>
2023-04-25 14:34:57 +0800

    feat: Update global layout page

--- src/Views/components/dashboard/sidebar.php
+++ src/Views/components/dashboard/sidebar.php
@@ -1,3 +1,9 @@
+<?php
+
+use NataInditama\Auctionx\App\Auth;
+
+$auth = $model['auth'] ?? Auth::getSession();
+?>
 <nav class="navbar-vertical navbar">
   <div class="nav-scroller">
     <a class="navbar-brand" href="./">
@@ -5,37 +11,34 @@
     </a>
     <ul class="navbar-nav flex-column" id="sideNavbar">
       <li class="nav-item">
-        <a class="nav-link has-arrow" href="./dashboard">
+        <a class="nav-link has-arrow" href="./">
           <i data-feather="home" class="nav-icon icon-xs me-2"></i> Dashboard
         </a>
       </li>
-      <?php if (isset($model['auth'])) : ?>
-        <?php if ($model['auth']['level'] == "administrator") :  ?>
-          <li class="nav-item">
-            <a class="nav-link has-arrow" href="./dashboard/auction">
-              <i data-feather="layers" class="nav-icon icon-xs me-2"> </i> Auctions
-            </a>
-          </li>
-        <?php elseif ($model['auth']['level'] == "petugas") :  ?>
-          <li class="nav-item">
-            <a class="nav-link has-arrow" href="./dashboard/auction">
-              <i data-feather="layers" class="nav-icon icon-xs me-2"> </i> Auctions
-            </a>
-          </li>
-        <?php elseif ($model['auth']['level'] == "masyarakat") :  ?>
-          <li class="nav-item">
-            <a class="nav-link has-arrow" href="./dashboard/history">
-              <i data-feather="dollar-sign" class="nav-icon icon-xs me-2"> </i> Penawaran
-            </a>
-          </li>
-        <?php endif; ?>
+      <li class="nav-item">
+        <a class="nav-link has-arrow" href="./auction">
+          <i data-feather="layers" class="nav-icon icon-xs me-2"> </i> Auction List
+        </a>
+      </li>
+      <?php if ($auth['level'] == "masyarakat") :  ?>
+        <li class="nav-item">
+          <a class="nav-link has-arrow" href="./bidding">
+            <i data-feather="dollar-sign" class="nav-icon icon-xs me-2"> </i> Bidding history
+          </a>
+        </li>
+      <?php elseif ($auth['level'] == "petugas") :  ?>
+        <li class="nav-item">
+          <a class="nav-link has-arrow" href="./">
+            <i data-feather="users" class="nav-icon icon-xs me-2"> </i> Subscriptions
+          </a>
+        </li>
       <?php endif; ?>
       <li class="nav-item">
         <div class="navbar-heading">Profile</div>
       </li>
       <li class="nav-item">
-        <a class="nav-link has-arrow" href="./dashboard/">
-          <i data-feather="user" class="nav-icon icon-xs me-2"> </i> Edit Profile
+        <a class="nav-link has-arrow" href="./profile">
+          <i data-feather="user" class="nav-icon icon-xs me-2"> </i> View Profile
         </a>
       </li>
       <li class="nav-item">
--- src/Views/components/footer.php
+++ src/Views/components/footer.php
@@ -1,7 +1,38 @@
-<?php if ($model['showSidebar'] ?? false) : ?>
-    </div>
+<?php
+
+use NataInditama\Auctionx\App\Auth;
+
+ if (Auth::getSession()) : ?>
+  </div>
   </div>
 <?php endif; ?>
+
+<footer class="footer bg-gray-900 py-8">
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-12 text-center">
+        <h2><a href="#" class="m-0 text-primary fw-bold"><?= SITE_NAME; ?></a></h2>
+        <div class="d-flex gap-3 justify-content-center align-items-center my-4">
+          <a class="text-light" href="./">Home</a>
+          <a class="text-light" href="./about">About</a>
+          <a class="text-light" href="./blog">News</a>
+          <a class="text-light" href="./contact">Contact</a>
+        </div>
+        <div class="ftco-footer-social p-0">
+          <li class="ftco-animate"><a href="#" class="border text-white" data-toggle="tooltip" data-placement="top" title="Twitter"><i data-feather="twitter" class="icon-xs" ></i></a></li>
+          <li class="ftco-animate"><a href="#" class="border text-white" data-toggle="tooltip" data-placement="top" title="Facebook"><span data-feather="facebook" class="icon-xs" ></span></a></li>
+          <li class="ftco-animate"><a href="#" class="border text-white" data-toggle="tooltip" data-placement="top" title="Instagram"><span data-feather="instagram" class="icon-xs" ></span></a></li>
+        </div>
+      </div>
+      <div class="row mt-5">
+        <div class="col-md-12 text-center">
+          <p class="copyright">
+            Copyright &copy;<?= date("Y") ?> All rights reserved
+          </p>
+        </div>
+      </div>
+    </div>
+</footer>
 </body>
 
 </html>
