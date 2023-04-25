48e46d2b0d2768d4f6f9c7f417b7e684f9c89320
Nata Inditama <natainditama@gmail.com>
2023-04-25 14:28:14 +0800

    feat: Implement data auction controllers

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -1,39 +1,73 @@
-<?php 
+<?php
 
 namespace NataInditama\Auctionx\Controllers;
 
+use NataInditama\Auctionx\App\Auth;
 use NataInditama\Auctionx\App\View;
 use NataInditama\Auctionx\Models\Barang;
+use NataInditama\Auctionx\Models\HistoryLelang;
+use NataInditama\Auctionx\Models\Lelang;
+use NataInditama\Auctionx\Models\Masyarakat;
 
 class HomeController
 {
+  private ?array $session;
+  public Lelang $auction;
   public Barang $product;
+  public Masyarakat $user;
+  public HistoryLelang $history;
 
   public function __construct()
   {
+    $this->session = Auth::getSession();
+    $this->auction = new Lelang();
     $this->product = new Barang();
+    $this->user = new Masyarakat();
+    $this->history = new HistoryLelang();
   }
 
   public function index(): void
   {
-    View::render("home/index");
-  }
-  
-  public function detail(string $id): void
-  {
-    var_dump($this->product->findById($id));
-    View::render("home/index", [
-      "title" => $id
-    ]);
+    if (isset($this->session)) {
+      if (isset($this->session['id_user']) && $this->session['level'] == "masyarakat") {
+        $auction = $this->auction->findAll();
+        $contribution = $this->history->findAllByUserId($this->session['id_user']);
+        $history = $this->history->findAllByUserId($this->session['id_user']);
+
+        $array = [];
+        foreach ($history as $key => $row) {
+          array_push($array, $row['penawaran_harga']);
+        }
+
+        $highest = number_format(!empty($array) ? max($array) : 0, 0, "", ".");
+        $total = number_format(array_sum($array), 2, ",", ".");
+        $avarage = number_format(!empty($array) ? array_sum($array) / count($array) : 0, 0, "", ".");
+        $avarageContribution = count($contribution) / count($auction) * 100;
+
+        View::render("dashboard/index", [
+          "auction" => $auction,
+          "contribution" => $avarageContribution,
+          "avarage" => $avarage,
+          "highest" => $highest,
+          "total" => $total,
+          "auth" => $this->session,
+        ]);
+      }
+
+      View::render("dashboard/index", [
+        "auth" => $this->session,
+      ]);
+    } else {
+      $auctions = $this->auction->findAllByStatus('dibuka');
+      View::render("auction/index", [
+        "products" => $auctions,
+        "auth" => $this->session,
+      ]);
+    }
   }
 
-  public function admin(): void
+  public function pricing(): void
   {
-    var_dump("admin");
-  }
-
-  public function login(): void
-  {
-    var_dump("login");
+    View::render("pricing");
   }
 }
