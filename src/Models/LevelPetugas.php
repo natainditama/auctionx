ed0b52d641cd5fd77e0b95bc2460605fef2974cc
Nata Inditama <natainditama@gmail.com>
2023-04-29 09:57:39 +0800

    refactor: Modify return types

--- src/Models/LevelPetugas.php
+++ src/Models/LevelPetugas.php
@@ -9,18 +9,29 @@ class LevelPetugas extends Database
   var int $id_level;
   var string $level;
 
-  public function findByIdLevel(int $levelId): ?LevelPetugas
+  public function findByIdLevel(int $levelId): ?array
   {
     $query = "SELECT `id_level`, `level` FROM `tb_level` WHERE `id_level` = ?";
 
     $statement = $this->mysqli->prepare($query);
-    $statement->bind_param("s", $levelId);
+    $statement->bind_param("i", $levelId);
     $statement->execute();
 
     $result = $statement->get_result();
-    return $result->fetch_object(LevelPetugas::class);
+    return $result->fetch_assoc();
   }
 
-  
+  public function findByLevel(string $level): ?array
+  { 
+    $query = "SELECT `id_level`, `level` FROM `tb_level` WHERE `level`=?";
+
+    $statement = $this->mysqli->prepare($query);
+    $statement->bind_param("s", $level);
+    $statement->execute();
+
+    $result = $statement->get_result();
+    return $result->fetch_assoc();
+
+  }
 
 }
