27d3665ac05fdab761b17ae48d88d7f924d1c7b1
Nata Inditama <natainditama@gmail.com>
2023-04-29 09:56:32 +0800

    feat: Implement CRUD users

--- src/Controllers/UserController.php
+++ src/Controllers/UserController.php
@@ -2,7 +2,10 @@
 
 namespace NataInditama\Auctionx\Controllers;
 
+use Exception;
 use NataInditama\Auctionx\App\Auth;
+use NataInditama\Auctionx\App\Database;
+use NataInditama\Auctionx\App\Flasher;
 use NataInditama\Auctionx\App\View;
 use NataInditama\Auctionx\Models\HistoryLelang;
 use NataInditama\Auctionx\Models\Lelang;
@@ -26,18 +29,22 @@ class UserController
     $this->auction = new Lelang();
   }
 
-  public function show(string $username, ?string $level = "masyarakat"): void
+  public function index(): void
   {
-    if ($level !== "masyarakat") {
-      $profile = (array) $this->admin->findByUsername($username);
-      $contributions = $this->auction->findAllByPetugasId($profile['id_petugas']);
-    } else {      
-      $profile = (array) $this->user->findByUsername($username);
-      $contributions = $this->history->findAllBindingHistoryByUserId($profile['id_user']);
-    }
+    $users = $this->user->findAll();
+    View::render("./user/index", [
+      "users" => $users,
+      "auth" => $this->session
+    ]);
+  }
+
+  public function show(string $username): void
+  {
+    $profile = $this->user->findByUsername($username);
+    $contributions = $this->history->findAllBindingHistoryByUserId($profile['id_user']);
 
     if (count($profile) > 0) {
-      View::render($view ?? "user/show", [
+      View::render("user/show", [
         "profile" => $profile,
         "history" => $contributions,
         "auth" => $this->session
@@ -47,13 +54,106 @@ class UserController
     View::render("404");
   }
 
-  public function profile(): void
-  {
-    $this->show($this->session['username'], $this->session['level']);
-  }
-
   public function setting(): void
   {
-    View::render("user/setting");
+    $profile = $this->user->findByUsername($this->session['username']);
+    View::render("user/setting", [
+      "auth" => $this->session,
+      "profile" => $profile
+    ]);
+  }
+
+  public function updateProfile(): void
+  {
+    $userId = $this->session['id_user'];
+    $this->update($userId, "profile");
+  }
+
+  public function profile(): void
+  {
+    $username = $this->session['username'];
+    $level = $this->session['level'];
+
+    if ($level !== "masyarakat") {
+      $profile = $this->admin->findByUsername($username);
+      $contributions = $this->auction->findAllByPetugasId($profile['id_petugas']);
+    } else {
+      $profile = $this->user->findByUsername($username);
+      $contributions = $this->history->findAllBindingHistoryByUserId($profile['id_user']);
+    }
+
+    if (count($profile) > 0) {
+      View::render("user/show", [
+        "profile" => $profile,
+        "history" => $contributions,
+        "auth" => $this->session
+      ]);
+      return;
+    }
+    View::render("404");
+  }
+  
+  public function update(string $userId, string $redirect = ""): void
+  {
+    $mysqli = Database::getConnection();
+    $mysqli->begin_transaction();
+    try {
+      $request = new Masyarakat();
+      $request->id_user = htmlspecialchars($userId);
+      $request->username = htmlspecialchars($_POST['username']);
+      $request->telp = htmlspecialchars($_POST['telp']);
+      $request->nama_lengkap = htmlspecialchars($_POST['name']);
+      $this->user->updateByUserId($request);
+
+      $profile = $this->user->findByUsername($this->session['username']);
+      Auth::setSession($profile);
+
+      $mysqli->commit();
+      Flasher::setFlasher("success", "Your request has been saved");
+      View::redirect($redirect ?? "./user");
+    } catch (Exception $th) {
+      $mysqli->rollback();
+      Flasher::setFlasher("danger", $th->getMessage());
+      View::redirect($redirect ?? "./user");
+    }
+  }
+
+  public function destroy(string $userId): void
+  {
+    $mysqli = Database::getConnection();
+    $mysqli->begin_transaction();
+    try {
+      $this->user->deleteByUserId($userId);
+
+      $mysqli->commit();
+      Flasher::setFlasher("warning", "Your request has been saved");
+      View::redirect("./user");
+    } catch (Exception $th) {
+      $mysqli->rollback();
+      Flasher::setFlasher("danger", $th->getMessage());
+      View::redirect("./user");
+    }
+  }
+
+  public function store(): void
+  {
+    $mysqli = Database::getConnection();
+    $mysqli->begin_transaction();
+    try {
+      $request = new Masyarakat();
+      $request->username = htmlspecialchars($_POST['username']);
+      $request->telp = htmlspecialchars($_POST['telp']);
+      $request->nama_lengkap = htmlspecialchars($_POST['name']);
+      $request->password = htmlspecialchars($_POST['password']);      
+      Auth::do_register($request);
+
+      $mysqli->commit();
+      Flasher::setFlasher("success", "Your request has been saved");
+      View::redirect("./user");
+    } catch (Exception $th) {
+      $mysqli->rollback();
+      Flasher::setFlasher("danger", $th->getMessage());
+      View::redirect("./user");
+    }
   }
 }
