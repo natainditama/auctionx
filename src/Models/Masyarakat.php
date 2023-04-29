e3331571e96ca0c5418d606bc091855dfc944124
Nata Inditama <natainditama@gmail.com>
2023-04-29 09:58:28 +0800

    feat: Add method update user

--- src/Models/Masyarakat.php
+++ src/Models/Masyarakat.php
@@ -2,6 +2,7 @@
 
 namespace NataInditama\Auctionx\Models;
 
+use Exception;
 use NataInditama\Auctionx\App\Database;
 
 class Masyarakat extends Database
@@ -42,5 +43,23 @@ class Masyarakat extends Database
 
     $result = $statement->get_result();
     return $result->fetch_all(MYSQLI_ASSOC);
+  }
+
+  public function updateByUserId(Masyarakat $data): void
+  {
+    $query = "UPDATE `tb_masyarakat` SET `nama_lengkap`=?,`username`=?,`telp`=? WHERE `id_user`=?";
+
+    $statement = $this->mysqli->prepare($query);
+    $statement->bind_param("sssi", $data->nama_lengkap, $data->username, $data->telp, $data->id_user);
+    $statement->execute();
+  }
+
+  public function deleteByUserId(string $userId): void
+  {
+    $query = "DELETE FROM `tb_masyarakat` WHERE `id_user`=?";
+
+    $statement = $this->mysqli->prepare($query);
+    $statement->bind_param('i', $userId);
+    $statement->execute();
   }
 }
