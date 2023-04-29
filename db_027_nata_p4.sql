79367d55838af776928581f3147b3193f32dc8dc
Nata Inditama <natainditama@gmail.com>
2023-04-29 15:29:08 +0800

    feat(database): Update database schema

--- db_027_nata_p4.sql
+++ db_027_nata_p4.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Apr 19, 2023 at 06:02 AM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.0.18
+-- Host: 127.0.0.1:3306
+-- Generation Time: Apr 29, 2023 at 09:24 AM
+-- Server version: 10.1.21-MariaDB
+-- PHP Version: 8.0.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -35,6 +35,22 @@ CREATE TABLE `history_lelang` (
   `penawaran_harga` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `history_lelang`
+--
+
+INSERT INTO `history_lelang` (`id_history`, `id_lelang`, `id_barang`, `id_user`, `penawaran_harga`) VALUES
+(1, 3, 3, 1, 300000),
+(2, 2, 2, 2, 650000),
+(3, 1, 1, 2, 700000),
+(4, 4, 4, 2, 320000),
+(5, 3, 3, 3, 350000),
+(6, 1, 1, 3, 750000),
+(7, 4, 4, 3, 400000),
+(8, 2, 2, 4, 720000),
+(11, 4, 4, 4, 450000),
+(12, 2, 2, 6, 800000);
+
 -- --------------------------------------------------------
 
 --
@@ -44,10 +60,20 @@ CREATE TABLE `history_lelang` (
 CREATE TABLE `tb_barang` (
   `id_barang` int(11) NOT NULL,
   `nama_barang` varchar(100) NOT NULL,
-  `tgl` date NOT NULL DEFAULT current_timestamp(),
+  `tgl` date NOT NULL,
   `harga_awal` int(20) NOT NULL,
   `deskripsi_barang` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_barang`
+--
+
+INSERT INTO `tb_barang` (`id_barang`, `nama_barang`, `tgl`, `harga_awal`, `deskripsi_barang`) VALUES
+(1, 'Ford Mustang', '2023-04-29', 400000, 'A charismatic sports car that gives you lots for your money'),
+(2, 'BMW i4', '2023-04-28', 600000, 'Capable electric coupe with lots of practicality'),
+(3, 'Lamborghini Aventador', '2023-04-27', 200000, 'A stunning supercar that sounds incredible'),
+(4, 'Lamborghini Urus', '2023-04-24', 290000, 'An Italian supercar in large SUV form');
 
 -- --------------------------------------------------------
 
@@ -58,13 +84,23 @@ CREATE TABLE `tb_barang` (
 CREATE TABLE `tb_lelang` (
   `id_lelang` int(11) NOT NULL,
   `id_barang` int(11) NOT NULL,
-  `tgl_dibuka` datetime NOT NULL DEFAULT current_timestamp(),
-  `tgl_ditutup` datetime NOT NULL DEFAULT current_timestamp(),
+  `tgl_dibuka` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `tgl_ditutup` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `harga_akhir` int(20) DEFAULT NULL,
   `id_user` int(11) DEFAULT NULL,
   `id_petugas` int(11) NOT NULL,
   `status` enum('dibuka','ditutup') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_lelang`
+--
+
+INSERT INTO `tb_lelang` (`id_lelang`, `id_barang`, `tgl_dibuka`, `tgl_ditutup`, `harga_akhir`, `id_user`, `id_petugas`, `status`) VALUES
+(1, 1, '2023-04-29 14:07:00', '2023-05-06 14:07:00', 750000, 3, 1, 'dibuka'),
+(2, 2, '2023-04-28 14:08:00', '2023-05-05 14:08:00', 800000, 6, 1, 'dibuka'),
+(3, 3, '2023-04-27 14:08:00', '2023-05-03 14:08:00', 350000, 3, 1, 'dibuka'),
+(4, 4, '2023-04-24 14:11:00', '2023-04-30 14:11:00', 450000, 4, 2, 'dibuka');
 
 -- --------------------------------------------------------
 
@@ -99,6 +135,18 @@ CREATE TABLE `tb_masyarakat` (
   `telp` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `tb_masyarakat`
+--
+
+INSERT INTO `tb_masyarakat` (`id_user`, `nama_lengkap`, `username`, `password`, `telp`) VALUES
+(1, 'Ryan Ary', 'ryan', '$2y$10$9bgyxhxAuI8hU.FwgF6vsuAR5oeIMozdo0Bykg1XOwpkT1uuChKDW', '123'),
+(2, 'Cempaka', 'cempaka', '$2y$10$JRStOsOacV2dxUB7MbKUxOtmeiL0GcGf49mCiqEBxLlGMCZ.vW03u', '123'),
+(3, 'Yunita', 'yunita', '$2y$10$nJ0oRjS.E5VgHiiTYFOPauOXo6NVg37DQ6lhZLd2Sccy9mM/5VRAq', '123'),
+(4, 'William', 'william', '$2y$10$Zq49COoJ4Ik5.xtAf07fze8LELI/XPoGeNmvUNrEsMmf7Y/Dr0ll6', '12131'),
+(5, 'dwik erika', 'dwik', '$2y$10$8M8rBnRDljNFkinatlAifOl02VB21Rf2TX24eaZvWDMEnqztNMTXC', '1234055050'),
+(6, 'Yovie', 'Yovie', '$2y$10$ngPVeFajnwZlg1yzBh.J7.aNxSsthvnVMbdZq3I1EU6ILRmBtD5Lu', '318030913');
+
 -- --------------------------------------------------------
 
 --
@@ -112,6 +160,14 @@ CREATE TABLE `tb_petugas` (
   `password` varchar(60) NOT NULL,
   `id_level` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_petugas`
+--
+
+INSERT INTO `tb_petugas` (`id_petugas`, `nama_petugas`, `username`, `password`, `id_level`) VALUES
+(1, 'Nata Inditama', 'natainditama', '$2y$10$xkHWEWW.c4qFk9hH1p/3aO5sbSkPCO9tRQ8Bw9/IVWXBkEWZBPv/C', 2),
+(2, 'dodek', 'dodek', '$2y$10$e8yJ2UtrNv3OZmbEl/kMkuqV2ASy2xEbyCMVVcOIGJDYaOV5O0r36', 1);
 
 --
 -- Indexes for dumped tables
@@ -170,37 +226,37 @@ ALTER TABLE `tb_petugas`
 -- AUTO_INCREMENT for table `history_lelang`
 --
 ALTER TABLE `history_lelang`
-  MODIFY `id_history` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=73;
+  MODIFY `id_history` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `tb_barang`
 --
 ALTER TABLE `tb_barang`
-  MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
+  MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `tb_lelang`
 --
 ALTER TABLE `tb_lelang`
-  MODIFY `id_lelang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id_lelang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `tb_level`
 --
 ALTER TABLE `tb_level`
-  MODIFY `id_level` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_level` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `tb_masyarakat`
 --
 ALTER TABLE `tb_masyarakat`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `tb_petugas`
 --
 ALTER TABLE `tb_petugas`
-  MODIFY `id_petugas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_petugas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
@@ -210,15 +266,15 @@ ALTER TABLE `tb_petugas`
 -- Constraints for table `history_lelang`
 --
 ALTER TABLE `history_lelang`
-  ADD CONSTRAINT `history_lelang_ibfk_1` FOREIGN KEY (`id_barang`) REFERENCES `tb_barang` (`id_barang`),
-  ADD CONSTRAINT `history_lelang_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `tb_masyarakat` (`id_user`),
+  ADD CONSTRAINT `history_lelang_ibfk_1` FOREIGN KEY (`id_barang`) REFERENCES `tb_barang` (`id_barang`) ON DELETE CASCADE,
+  ADD CONSTRAINT `history_lelang_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `tb_masyarakat` (`id_user`) ON DELETE CASCADE,
   ADD CONSTRAINT `history_lelang_ibfk_3` FOREIGN KEY (`id_lelang`) REFERENCES `tb_lelang` (`id_lelang`);
 
 --
 -- Constraints for table `tb_lelang`
 --
 ALTER TABLE `tb_lelang`
-  ADD CONSTRAINT `tb_lelang_ibfk_1` FOREIGN KEY (`id_barang`) REFERENCES `tb_barang` (`id_barang`),
+  ADD CONSTRAINT `tb_lelang_ibfk_1` FOREIGN KEY (`id_barang`) REFERENCES `tb_barang` (`id_barang`) ON DELETE CASCADE,
   ADD CONSTRAINT `tb_lelang_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `tb_masyarakat` (`id_user`),
   ADD CONSTRAINT `tb_lelang_ibfk_3` FOREIGN KEY (`id_petugas`) REFERENCES `tb_petugas` (`id_petugas`);
 
