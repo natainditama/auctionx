bec5ca6e6cf962e199999f537b33b0b248481901
Nata Inditama <natainditama@gmail.com>
2023-05-21 05:02:40 +0800

    fix: Unsupport named arguments

--- public/index.php
+++ public/index.php
@@ -4,6 +4,7 @@ session_start();
 require_once __DIR__ . "/../src/config.php";
 require_once __DIR__ . "/../vendor/autoload.php";
 
+
 use NataInditama\Auctionx\App\Router;
 use NataInditama\Auctionx\Controllers\AuctionController;
 use NataInditama\Auctionx\Controllers\AuthController;
@@ -15,43 +16,43 @@ use NataInditama\Auctionx\Middleware\BidderMiddleware;
 use NataInditama\Auctionx\Middleware\GuestMiddleware;
 use NataInditama\Auctionx\Middleware\SellerMiddleware;
 
-// Home Routes
-Router::add(path: '/', controller: HomeController::class, function: 'index');
-Router::add(path: '/pricing', controller: HomeController::class, function: 'pricing');
+// // Home Routes
+Router::add('/', HomeController::class, 'index');
+Router::add('/pricing', HomeController::class, 'pricing');
 
 // Auth Routes
-Router::add(path: '/logout', controller: AuthController::class, function: 'logout', middleware: [GuestMiddleware::class]);
-Router::add(path: '/login', controller: AuthController::class, function: 'login', middleware: [SellerMiddleware::class]);
-Router::add(path: '/register', controller: AuthController::class, function: 'register', middleware: [AuthMiddleware::class]);
-Router::add(path: '/premium/register', controller: AuthController::class, function: 'adminRegister', middleware: [SellerMiddleware::class]);
-Router::add(path: '/login', controller: AuthController::class, function: 'postLogin', method: "POST");
-Router::add(path: '/register', controller: AuthController::class, function: 'postRegister', method: "POST");
+Router::add('/logout', AuthController::class, 'logout', "GET", [GuestMiddleware::class]);
+Router::add('/login', AuthController::class, 'login', "GET", [SellerMiddleware::class]);
+Router::add('/register', AuthController::class, 'register', "GET", [AuthMiddleware::class]);
+Router::add('/premium/register', AuthController::class, 'adminRegister', "GET", [SellerMiddleware::class]);
+Router::add('/login', AuthController::class, 'postLogin', "POST");
+Router::add('/register', AuthController::class, 'postRegister', "POST");
 
 // Auction Routes
-Router::add(path: '/auction', controller: AuctionController::class, function: 'index');
-Router::add(path: '/auction', controller: AuctionController::class, function: 'store', method: "POST");
-Router::add(path: '/auction/export', controller: AuctionController::class, function: 'export', middleware: [BidderMiddleware::class]);
-Router::add(path: '/auction/create', controller: AuctionController::class, function: 'create', middleware: [GuestMiddleware::class, BidderMiddleware::class]);
-Router::add(path: '/auction/([0-9]*)', controller: AuctionController::class, function: 'show');
-Router::add(path: '/auction/([0-9]*)', controller: AuctionController::class, function: 'bid', method: "POST");
-Router::add(path: '/auction/([0-9]*)/edit', controller: AuctionController::class, function: 'edit', middleware: [GuestMiddleware::class, BidderMiddleware::class]);
-Router::add(path: '/auction/([0-9]*)/update', controller: AuctionController::class, function: 'update', method: "POST");
-Router::add(path: '/auction/([0-9]*)/destroy', controller: AuctionController::class, function: 'destroy', method: "POST");
-Router::add(path: '/auction/([0-9]*)/contributions', controller: AuctionController::class, function: 'contributions');
+Router::add('/auction', AuctionController::class, 'index');
+Router::add('/auction', AuctionController::class, 'store', "POST");
+Router::add('/auction/export', AuctionController::class, 'export', "GET", [BidderMiddleware::class]);
+Router::add('/auction/create', AuctionController::class, 'create', "GET", [GuestMiddleware::class, BidderMiddleware::class]);
+Router::add('/auction/([0-9]*)', AuctionController::class, 'show');
+Router::add('/auction/([0-9]*)', AuctionController::class, 'bid', "POST");
+Router::add('/auction/([0-9]*)/edit', AuctionController::class, 'edit', "GET", [GuestMiddleware::class, BidderMiddleware::class]);
+Router::add('/auction/([0-9]*)/update', AuctionController::class, 'update', "POST");
+Router::add('/auction/([0-9]*)/destroy', AuctionController::class, 'destroy', "POST");
+Router::add('/auction/([0-9]*)/contributions', AuctionController::class, 'contributions');
 
 // Bidding Routes
-Router::add(path: '/bidding', controller: BiddingController::class, function: 'index', middleware: [GuestMiddleware::class, SellerMiddleware::class]);
-Router::add(path: '/bidding/([0-9]*)/destroy', controller: BiddingController::class, function: 'destroy', method: "POST");
+Router::add('/bidding', BiddingController::class, 'index', "GET", [GuestMiddleware::class, SellerMiddleware::class]);
+Router::add('/bidding/([0-9]*)/destroy', BiddingController::class, 'destroy', "POST");
 
 // User Routes
-Router::add(path: "/profile", controller: UserController::class, function: 'profile', middleware: [GuestMiddleware::class]);
-Router::add(path: "/setting", controller: UserController::class, function: 'setting', middleware: [GuestMiddleware::class]);
-Router::add(path: "/setting", controller: UserController::class, function: 'updateProfile', method: "POST");
-Router::add(path: "/user", controller: UserController::class, function: 'index', middleware: [BidderMiddleware::class]);
-Router::add(path: '/user', controller: UserController::class, function: 'store', method: "POST");
-Router::add(path: '/user/([0-9a-zA-Z]*)', controller: UserController::class, function: 'show');
-Router::add(path: '/user/([0-9a-zA-Z]*)/update', controller: UserController::class, function: 'update', method: "POST");
-Router::add(path: '/user/([0-9a-zA-Z]*)/destroy', controller: UserController::class, function: 'destroy', method: "POST");
+Router::add("/profile", UserController::class, 'profile', "GET", [GuestMiddleware::class]);
+Router::add("/setting", UserController::class, 'setting', "GET", [GuestMiddleware::class]);
+Router::add("/setting", UserController::class, 'updateProfile', "POST");
+Router::add("/user", UserController::class, 'index', "GET", [BidderMiddleware::class]);
+Router::add('/user', UserController::class, 'store', "POST");
+Router::add('/user/([0-9a-zA-Z]*)', UserController::class, 'show');
+Router::add('/user/([0-9a-zA-Z]*)/update', UserController::class, 'update', "POST");
+Router::add('/user/([0-9a-zA-Z]*)/destroy', UserController::class, 'destroy', "POST");
 
 
 Router::run();
